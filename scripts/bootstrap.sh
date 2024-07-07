5b3ffc249fb3529f1b2633f8de9903b14bc18138
mocul <823309460@qq.com>
2024-07-08 02:26:04 +0800

    [2024-07-08][mocul](fix): 1. add git submodule update in bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,10 +1,12 @@
 #!/bin/env bash
+
+git submodule update --init
+
 SCRIPT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 BOARDS_ARRAY=$(grep 'BOARDS=' "${SCRIPT_DIR}/../config/boards.cfg")
 SPILT=$(echo "$BOARDS_ARRAY" | sed -e 's/BOARDS=\[//' -e 's/\]//')
 SPILT=$(echo "$SPILT" | tr -d ' ' | tr ',' '\n')
 IFS=$'\n' read -rd '' -a BOARDS <<< "$SPILT"
-
 for BOARD in "${BOARDS[@]}"; do
     TARGET_DIR=$(find "${SCRIPT_DIR}/../boards/" -type d -name "${BOARD}" -print -quit)
     if [ -n "${TARGET_DIR}" ]; then
